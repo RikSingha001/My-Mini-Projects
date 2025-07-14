4bdd8a9b4dd1d825eba3e2db7bd70de7ba031a70
Rik-Singha <riksingha420@gmail.com>
2025-07-14 22:55:51 +0530

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const resultDisplay = document.getElementById('result');
 const end = document.getElementById('end');
 const tilesize=20;
 const tileCount = canvas.width / tilesize;
-let snake = [{ x: 510, y: 510 }];
+let snake = [{ x: 5, y: 5 }];
 let direction = { x: 1, y: 0 };
 let food = randomFood();
 let result =0;
@@ -108,7 +108,7 @@ document.addEventListener("keydown",(e)=>{
 
 });
 function restartGame(){
-  snake= [{x:510,y:510}];
+  snake= [{x:5,y:5}];
   direction={x:1,y:0};
   food=randomFood();
   gameOver=false;
@@ -116,4 +116,6 @@ function restartGame(){
   requestAnimationFrame(gameLoop);
   }
     requestAnimationFrame(gameLoop);
+
+//npx http-server -p 3000
 
