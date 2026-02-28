a5c5f4070bb624b8aaa4dae14a1a9e6f77089724
Rik-Singha <riksingha420@gmail.com>
2026-02-28 17:25:12 +0530

    update

--- django_api_chatbot/config/settings.py
+++ django_api_chatbot/config/settings.py
@@ -145,11 +145,9 @@ AUTH_USER_MODEL = 'hanabi.User'
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
