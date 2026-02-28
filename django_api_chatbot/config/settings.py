e62cda9471a25dd347e589237e2bc89deef13d64
Rik-Singha <riksingha420@gmail.com>
2026-02-28 17:27:56 +0530

    update

--- django_api_chatbot/config/settings.py
+++ django_api_chatbot/config/settings.py
@@ -151,7 +151,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
