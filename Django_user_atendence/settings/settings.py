73e10acfcc3903959721dbcbd358f4a63bf9692a
Rik-Singha <riksingha420@gmail.com>
2026-08-16 16:04:47 +0530

    update

--- Django_user_atendence/settings/settings.py
+++ Django_user_atendence/settings/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-x=b-5fwz(6vsv62h3si=9ku3zqy4qd!@31bbr(=7h)nks8hxk4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
